c5f2a92252336ce89065191f41e027556369cd2d
Anu <anupambhartiab@gmail.com>
2024-07-29 02:19:24 +0530

    Initial Commit

---
+++ Anupam_iOS_Xeta/Anupam_iOS_XetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anupam_iOS_XetaApp.swift
+//  Anupam_iOS_Xeta
+//
+//  Created by Anu on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Anupam_iOS_XetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
